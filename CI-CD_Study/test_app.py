d4392876bafa2319677ddbf530b23570e25e470b
Harun Taha Kepenek <hakukpnk@gmail.com>
2024-12-14 22:07:11 +0300

    deneme3

--- CI-CD_Study/test_app.py
+++ CI-CD_Study/test_app.py
@@ -3,8 +3,8 @@ from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, harung!")
-        self.assertEqual(greet("Python"), "Hello, asd!")
+        self.assertEqual(greet("World"), "Hello, World!")
+        self.assertEqual(greet("Python"), "Hello, Python!")
 
 if __name__ == "__main__":
     unittest.main()
